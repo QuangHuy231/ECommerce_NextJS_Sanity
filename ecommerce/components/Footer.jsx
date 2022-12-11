3d43c6052c9783ab2c5235b28503beaf0de6b13b
Quang Huy <nguyenquanghuya3kd@gmail.com>
2022-12-11 17:00:35 +0700

    create Layout : Navbar and Footer

--- ecommerce/components/Footer.jsx
+++ ecommerce/components/Footer.jsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+import { AiFillInstagram, AiFillTwitterCircle } from "react-icons/ai";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer-container">
+      <p>2022 All rights reserverd</p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiFillTwitterCircle />
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
